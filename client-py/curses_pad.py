94a74b4066ce332d649add7df05819c6e0ae848c
Douglas Hutchings <11douglash@berkeley.edu>
2016-03-01 13:02:43 -0800

    Initial commit of curses_pad
    curses_pad.py does what I want it to.  Namely:
    
    Resisable command output
    Single line at the bottom of screen is text input
    and it handles backspaces.

---
+++ client-py/curses_pad.py
@@ -0,0 +1,152 @@
+import curses
+import time
+
+MAX_CONSOLE_LINES = 10
+#maximum number of lines to show on the console.
+
+
+stdscr = None
+console_lines = ["" for _ in range(MAX_CONSOLE_LINES)]
+input_line = ""
+
+height, width = 10,10 #just initial guesses.  They'll be overwritten soon.
+
+
+def test_code():
+    while True:
+    
+        user_input()     
+        show()
+        #loop_ct += 1
+
+def init(stdscr_in):
+    global stdscr
+    stdscr = stdscr_in
+    stdscr.nodelay(1)
+
+
+    test_code()
+
+
+def user_input():
+    global stdscr #get the global curses instance.
+    global send_user_in
+    #get lines in.  Update handles displaying approprpriately.
+
+    while True:
+      c = stdscr.getch()
+      if c < 0:
+        #i ran out of characters to get
+        break
+      elif 32 <= c <= 127:
+        #append the human-readable string.  Not sure if i should inlucde 127 (delete), but nikita does.
+        add_input_char(chr(c))
+      elif c == 10:
+        #user hits 'return'  
+        clear_input_line()
+      elif c == 263: #apparently backspace is 263 in curses land, not 8
+        delete_input_char()
+        #user just hit 'delete'
+    stdscr.refresh()
+
+
+def show_input_line(text):
+    #update input line to read "text"
+    global height
+    global width
+
+
+
+    input_win = curses.newwin(1, width, height - 1, 0)
+    input_win.addstr(0, 0, ">>> " + text)
+    input_win.refresh()
+
+def show_console(lines):
+    #show 'lines' on the console
+    global width
+    global height
+
+    console_pad = curses.newpad(100, width)
+
+    for i, line in enumerate(lines[-(MAX_CONSOLE_LINES-1):]):
+        #print i, line
+        #print(i)
+        #print(line.strip())
+        console_pad.addstr(i+1, 0, line.strip())
+
+    #fill the console with junk for testing
+    #for y in range(0, 99):
+    #    for x in range(0, width-1):
+    #        console_pad.addch(y,x, ord('a') + (x*x+y*y) % 26)
+
+    y1 = 0 #top of screen
+    x1 = 0 #left of the screen
+    y2 = height -1 #so we don't run off the end
+    x2 = width -1 #to all the way on the right, minus 1
+    
+    time.sleep(.05) #needed to prevent flickering.  Don't know why.
+    console_pad.refresh(0,0, y1,x1,y2,x2)
+
+
+
+def add_console_line(newline):
+    #add newline to the currently existing console output
+    #does not handle showing operations
+    global console_lines
+    console_lines.append(newline)
+    console_lines = console_lines[-MAX_CONSOLE_LINES:]
+
+def add_input_char(newChar):
+    global input_line
+    input_line = input_line + newChar
+    #add input_character to the currently existing (and growing) line on the bottom of the screen.
+
+def delete_input_char():
+    #user just hit backspace key, let's delete the line.
+    global input_line
+    input_line = input_line[:-1]
+
+
+def clear_input_line():
+    global console_lines
+    global input_line
+    add_console_line(input_line)
+    input_line = ""
+    #input line gets added to console area, cleared.
+
+def show():
+
+    global height
+    global width
+    global MAX_CONSOLE_LINES
+    global console_lines
+
+    new_height, new_width = stdscr.getmaxyx()    #do this so screen will dynamically resise, if I do my job right.
+
+
+    #and the size of my console also dynamically resises
+    if new_height > height:
+        #need to append new lines to console_lines, at the front.
+        for i in range(new_height - height):
+            console_lines.insert(0,"")
+        MAX_CONSOLE_LINES = new_height - 1
+    elif new_height < height:
+        #I shrunk, need to trim console_lines
+        MAX_CONSOLE_LINES = new_height - 1
+        console_lines = console_lines[-MAX_CONSOLE_LINES:]
+    height = new_height
+    width = new_width
+
+
+
+    show_input_line(input_line)
+    show_console(console_lines)
+
+
+    stdscr.refresh()
+    #show updates
+
+if __name__ == "__main__":
+    print("Hello World")
+    curses.wrapper(lambda stdscr: init(stdscr))#start up user_input
+
